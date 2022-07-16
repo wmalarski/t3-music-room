99ff85beeac578a4cf89605bb843feb8193b61eb
Wojciech Malarski <w.malarski@gmail.com>
2022-07-16 11:10:17 +0200

    removing unused routers

--- src/server/router/auth.ts
+++
@@ -1,7 +0,0 @@
-import { createRouter } from "./context";
-
-export const authRouter = createRouter().query("getSession", {
-  resolve({ ctx }) {
-    return ctx.session;
-  },
-});
--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,14 +1,10 @@
 import superjson from "superjson";
 import { createRouter } from "./context";
 
-import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
 import { roomsRouter } from "./rooms";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter)
-  .merge("auth.", authRouter)
   .merge("rooms.", roomsRouter);
 
 // export type definition of API
