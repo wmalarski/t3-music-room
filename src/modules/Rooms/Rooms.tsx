75dba85ed21c060a19f54d94f13dc820b75586fa
Wojciech Malarski <w.malarski@gmail.com>
2022-07-21 20:52:12 +0200

    rooms list moves

--- src/modules/Rooms/Rooms.tsx
+++ src/modules/Rooms/Rooms.tsx
@@ -1,58 +1,12 @@
-import { Spinner, VStack } from "@chakra-ui/react";
-import { Pagination } from "@components/Pagination/Pagination";
-import { ResultMessage } from "@components/ResultMessage/ResultMessage";
-import { paths } from "@utils/paths";
-import { trpc } from "@utils/trpc";
-import Link from "next/link";
-import { ReactElement, useState } from "react";
+import { VStack } from "@chakra-ui/react";
+import { ReactElement } from "react";
 import { CreateRoomModal } from "./CreateRoomModal/CreateRoomModal";
+import { RoomsList } from "./RoomsList/RoomsList";
 
 export const Rooms = (): ReactElement => {
-  const [page, setPage] = useState(0);
-  const take = 100;
-
-  const client = trpc.useContext();
-
-  const query = trpc.proxy.members.selectMyMembers.useQuery(
-    { skip: page * take, take },
-    {
-      onSuccess: ([data]) => {
-        data.forEach((member) => {
-          client.setQueryData(
-            ["members.selectMemberByRoomId", { roomId: member.room.id }],
-            member
-          );
-        });
-      },
-    }
-  );
-
-  if (query.status === "loading" || query.status === "idle") {
-    return <Spinner />;
-  }
-
-  if (query.status === "error") {
-    return <ResultMessage message={query.error.message} variant="error" />;
-  }
-
-  const [members, maxSize] = query.data;
-
-  if (members.length <= 0) {
-    return <ResultMessage variant="empty" />;
-  }
-
   return (
     <VStack>
-      {members.map((member) => (
-        <Link href={paths.room(member.room.id)} key={member.room.id}>
-          {member.room.name}
-        </Link>
-      ))}
-      <Pagination
-        current={page}
-        maxPage={Math.ceil(maxSize / take)}
-        onChange={setPage}
-      />
+      <RoomsList />
       <CreateRoomModal />
     </VStack>
   );
